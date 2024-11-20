35174fdad10270b2e899117606537e0e468ecd35
extreme4all <40169115+extreme4all@users.noreply.github.com>
2024-11-20 22:17:20 +0100

    fix bug

--- src/jobs/tipoff/queries.py
+++ src/jobs/tipoff/queries.py
@@ -9,7 +9,6 @@ TIPOFF_CONFIG = """
     FROM Players as pl
     INNER JOIN Predictions as p on (p.id = pl.id)
     INNER JOIN reportLatest as rp on (rp.reported_id = pl.id)
-    INNER JOIN playerHiscoreDataLatest as phd on phd.Player_id = pl.id
     INNER JOIN regionIDNames as rid on rid.region_ID = rp.region_ID
     WHERE 1=1
         and p.Predicted_confidence > 99
